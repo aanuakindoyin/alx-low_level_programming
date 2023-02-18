c473efc8d39479894a696d758e2ab54c6a8b0661
aanuakindoyin <aanuakindoyin@gmail.com>
2023-02-18 02:26:21 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * Description: main - print declared char string to std output.
+ * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
