79c1ba0fb73b5e07ad6d750c6157e8015b2a19f3
aanuakindoyin <aanuakindoyin@gmail.com>
2023-02-18 02:27:49 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
+ * Description: main - Puts out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
