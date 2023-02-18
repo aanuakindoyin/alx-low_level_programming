1db5fac66832018eee19a09c1ef5c3b8ad867a4f
aanuakindoyin <aanuakindoyin@gmail.com>
2023-02-18 02:24:27 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * Description: main - print declared char string to std output.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
